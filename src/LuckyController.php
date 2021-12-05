d9813ae4db0730eb339f9f534472e250df522423
Sergej V <vasiliokas@gmail.com>
2021-12-05 23:10:00 +0000

    html/luck cont

--- src/LuckyController.php
+++ src/LuckyController.php
@@ -1,9 +1,12 @@
 <?php
 
-namespace App\Controller;
-// For using annotations 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+namespace App\Controller    ;
 
+// For using annotations 
+// use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class LuckyController 
@@ -18,6 +21,10 @@ class LuckyController
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
         );
+
+        // return $this->render('lucky/number.html.twig', [
+        //     'number' => $number,
+        // ]);
     }
 
 }
