5cf913f37b9a9440665eb460532b7edadf20ec23
Sergej <vasiliokas@gmail.com>
2021-12-06 16:48:41 +0000

    lazy with Maker bundle

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
+// var_dump( new Request);
+dump( new Request);
+// die('test');
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
