5b0a53ccafa8cd848bec52f8e50b1deedfed1f83
Sergej <vasiliokas@gmail.com>
2021-12-06 16:55:07 +0000

    remove debug

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 // var_dump( new Request);
-dump( new Request);
+// dump( new Request);
 // die('test');
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
