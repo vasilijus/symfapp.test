0b04db8a20634527b25bfd78d674119a3809efe6
Sergej V <vasiliokas@gmail.com>
2021-12-06 21:03:29 +0000

    ConfCont output

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +13,25 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/conference", name="conference")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]);
+        // return $this->render('conference/index.html.twig', [
+        //     'controller_name' => 'ConferenceController',
+        // ]); 
+        $greet = '';
+
+        if ($name = $request->query->get('hello') ) {
+            $greet = sprintf("<h1>Hello %s</h1>", htmlspecialchars($name));
+            $request->query->set('goodbye', 'Goodbye '.$name);
+        } 
+        
+        return new Response(
+            <<<EOF
+            <html>
+            <h1>Conference</h1>
+            $greet
+            </html>
+            EOF
+        );
     }
 }
