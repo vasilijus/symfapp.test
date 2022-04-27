126197af19a4573270455e6895509e73b0b98303
Sergej V <vasiliokas@gmail.com>
2022-04-27 17:13:47 +0100

    var-dumper

---
+++ src/Controller/MainController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    public $class_Name = __CLASS__;
+
+    #[Route('/main', name: 'main')]
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => $this->class_Name,
+        ]);
+        // return $this->json([
+        //    'controller_name' => $this->class_Name 
+        // ]);
+    }
+
+    #[Route('/main-2', name: 'main2')]
+    public function main2(): Response
+    {
+        return new Response('main 2');
+    }
+    #[Route('/main-3/{name?}', name: 'main2')]
+    public function main3( Request $request )
+    {
+        // dump($request);
+        return $request;
+    }
+}
